37c8230f6ca5c7e2b9c2238c05e54e26b3aa6caa
Andy Hotzler <ahotzler@gmail.com>
2021-04-26 20:37:18 -0500

    All tests passing; deployed to Vercel; full functionality

--- front-end/src/dashboard/DashboardList.js
+++ front-end/src/dashboard/DashboardList.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 export default function DashboardList({ reservation }) {
 
+    // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+
     const cancelHandler = (event) => {
         event.preventDefault();
 
